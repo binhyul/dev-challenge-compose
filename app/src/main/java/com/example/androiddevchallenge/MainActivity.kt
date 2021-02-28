14034a2a7e63fe47c9d0bb45a7d178a2f42aaceb
hjcho <hjcho@qualson.com>
2021-03-01 04:38:59 +0900

    feat: make first list for puppy items

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,13 +16,32 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.GravityCompat
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -36,13 +55,96 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+data class PuppyItem(
+    val name : String,
+    val thumbnail : Int,
+    val postDate : String,
+    val puppyImage : Int
+)
+
+
 // Start building your app here!
 @Composable
 fun MyApp() {
+    val dogItemOnClick = object : () -> Unit{
+        override fun invoke() {
+            Log.e("Logging on click","")
+        }
+    }
+    val dogItems  : List<PuppyItem> = listOf(
+        PuppyItem("Han Jin",R.drawable.img_1428,"2020/02/28",0),
+        PuppyItem("Han Ju",R.drawable.img_0636,"2020/02/28",0),
+        PuppyItem("Han Jun",R.drawable.img_0805,"2020/02/28",0),
+        PuppyItem("Jin Ju Jun",R.drawable.img_1423,"2020/02/28",0)
+    )
+
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Column(modifier = Modifier
+            .background(colorResource(id = R.color.color_back))
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Selling a Puppy")
+            LazyColumn(
+                content = {
+                items(dogItems){ item ->
+                    DogItem(model = item,onClick = { dogItemOnClick() })
+                }
+            })
+        }
     }
 }
+
+@Composable
+fun DogItem(model : PuppyItem,onClick : () -> Unit){
+    val padding = 16.dp
+    val margin= 16.dp
+    val thumbnailSize  = 72.dp
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .padding(padding),elevation = 4.dp) {
+        Column(
+            Modifier
+                .clickable { onClick() }
+                .clipToBounds()
+                .fillMaxWidth()
+                .padding(padding)
+                .background(colorResource(id = R.color.material_on_primary_emphasis_medium))
+        ) {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = model.thumbnail),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(thumbnailSize, thumbnailSize)
+                        .clip(CircleShape)
+                        .clipToBounds(),
+                    contentScale = ContentScale.FillBounds)
+                Column(verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .padding(margin, 0.dp, 0.dp, 0.dp)) {
+                    Text(text = "${model.name}")
+                    Text(text = "${model.postDate}")
+                }
+            }
+            Image(
+                painter = painterResource(id = model.thumbnail),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 10.dp, 0.dp, 0.dp)
+                    .background(colorResource(id = R.color.color_3)),
+            contentScale = ContentScale.Fit)
+                }
+    }
+}
+
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
@@ -52,10 +154,10 @@ fun LightPreview() {
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
-    }
-}
+//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+//@Composable
+//fun DarkPreview() {
+//    MyTheme(darkTheme = true) {
+//        MyApp()
+//    }
+//}
